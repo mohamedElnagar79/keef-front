e6cc6c1b1a3b7758526ddede55b1314287b70260
mohamedElnagar97 <mohamedelnaggar002@gmail.com>
2022-10-03 13:44:29 +0200

    handel validation

--- src/app/core/sign-in/sign-in.component.ts
+++ src/app/core/sign-in/sign-in.component.ts
@@ -10,6 +10,7 @@ import { Login } from 'src/app/_models/login';
 })
 export class SignInComponent implements OnInit {
   login: Login = new Login('', '');
+  loginFlag: boolean = false;
   constructor(
     private elogin: LoginService,
     private ac: ActivatedRoute,
@@ -20,8 +21,17 @@ export class SignInComponent implements OnInit {
   ngOnInit(): void {}
   loginSubmit() {
     // this.root.navigateByUrl("/students")
-    this.elogin.addservice(this.login).subscribe((a) => {
-      console.log('=====>', a);
-    });
+    this.elogin.addservice(this.login).subscribe(
+      (a) => {
+        console.log('=====>', a);
+        (this.loginFlag = false), this.router.navigateByUrl('/home');
+      },
+      (e) => {
+        console.log('error=====>', e.error.data);
+        this.loginFlag = true;
+        this.login.email = '';
+        this.login.password = '';
+      }
+    );
   }
 }
